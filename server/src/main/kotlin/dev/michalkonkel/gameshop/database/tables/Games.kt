a7be1e874d30e2cdb4d9e9112348350e84f03cd8
Michał Konkel <Michal.Konkel@velobank.pl>
2024-01-02 05:30:26 +0100

    feat: [Ktor Client] Refactor of Games.kt, introducing GamesEntity

--- server/src/main/kotlin/dev/michalkonkel/gameshop/database/tables/Games.kt
+++ server/src/main/kotlin/dev/michalkonkel/gameshop/database/tables/Games.kt
@@ -1,9 +1,21 @@
 package dev.michalkonkel.gameshop.database.tables
 
+import org.jetbrains.exposed.dao.UUIDEntity
+import org.jetbrains.exposed.dao.UUIDEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
+import java.util.UUID
 
 object Games : UUIDTable() {
     val name = varchar("name", 128)
     val description = varchar("description", 128)
     val price = varchar("price", 128)
+}
+
+class GamesEntity(id: EntityID<UUID>) : UUIDEntity(id) {
+    companion object : UUIDEntityClass<GamesEntity>(Games)
+
+    var name by Games.name
+    var description by Games.description
+    var price by Games.price
 }
--- server/src/main/kotlin/dev/michalkonkel/gameshop/features/games/data/RealGamesDAOFacade.kt
+++ server/src/main/kotlin/dev/michalkonkel/gameshop/features/games/data/RealGamesDAOFacade.kt
@@ -1,49 +1,40 @@
 package dev.michalkonkel.gameshop.features.games.data
 
 import dev.michalkonkel.gameshop.database.dbQuery
-import dev.michalkonkel.gameshop.database.tables.Games
+import dev.michalkonkel.gameshop.database.tables.GamesEntity
 import dev.michalkonkel.gameshop.domain.games.Game
 import dev.michalkonkel.gameshop.domain.games.GameRequest
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
 import java.util.UUID
 
 class RealGamesDAOFacade : GamesDAOFacade {
-    override suspend fun createGame(gameRequest: GameRequest) =
-        dbQuery {
-            Games
-                .insert {
-                    it[name] = gameRequest.name
-                    it[description] = gameRequest.description
-                    it[price] = gameRequest.price
-                }.resultedValues
-                ?.singleOrNull()
-                ?.toGame()
-        }
+    override suspend fun createGame(gameRequest: GameRequest) = dbQuery {
+        GamesEntity
+            .new {
+                name = gameRequest.name
+                description = gameRequest.description
+                price = gameRequest.price
+            }
+            .toGame()
+    }
 
-    override suspend fun getGame(id: String): Game? =
-        dbQuery {
-            Games
-                .select { Games.id eq UUID.fromString(id) }
-                .limit(1)
-                .firstOrNull()
-                ?.toGame()
-        }
+    override suspend fun getGame(id: String): Game? = dbQuery {
+        GamesEntity
+            .findById(UUID.fromString(id))
+            ?.toGame()
+    }
 
     override suspend fun getGames(): List<Game> =
         dbQuery {
-            Games.selectAll().map { it.toGame() }
+            GamesEntity.all().map { it.toGame() }
         }
 
-    private fun ResultRow.toGame(): Game =
+    private fun GamesEntity.toGame(): Game =
         this.let {
             Game(
-                id = it[Games.id].toString(),
-                name = it[Games.name],
-                description = it[Games.description],
-                price = it[Games.price],
+                id = it.id.value.toString(),
+                name = it.name,
+                description = it.description,
+                price = it.price,
             )
         }
 }
